ec42e6e0b45cd9b0666cfc83d1c3ada880674633
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-07-31 21:53:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,16 @@ class_dict = {"0": "Iris setosa",
 st.title("Iris - Model prediction")
 st.subheader('Power by:')
 st.markdown("""[Héctor Chocobar Torrejón](https://chocobar.net)""")
+st.divider()
 
-val1 = st.slider("Petal width", min_value = 0.1, max_value = 2.5, step = 0.1)
-val2 = st.slider("Petal length", min_value = 1.0, max_value = 6.9, step = 0.1)
-val3 = st.slider("Sepal width", min_value = 2.0, max_value = 4.4, step = 0.1)
-val4 = st.slider("Sepal length", min_value = 4.3, max_value = 7.9, step = 0.1)
+val1 = st.slider("Petal width", min_value=0.1, max_value=2.5, step=0.1)
+val2 = st.slider("Petal length", min_value=1.0, max_value=6.9, step=0.1)
+val3 = st.slider("Sepal width", min_value=2.0, max_value=4.4, step=0.1)
+val4 = st.slider("Sepal length", min_value=4.3, max_value=7.9, step=0.1)
 
 if st.button("Predict"):
     prediction = str(model.predict([[val1, val2, val3, val4]])[0])
     pred_class = class_dict[prediction]
+    st.divider()
     st.write("Prediction:", pred_class)
+    st.divider()
